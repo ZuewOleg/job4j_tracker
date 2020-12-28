40d924542bc71354b7855a2a5ed5c1f0c333fde4
ZuewOleg <zuev_17@bk.ru>
2020-12-28 15:29:02 +0500

    2. Перегрузка конструктора [#414142]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,22 @@ public class Item {
     private int id;
     private String name;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Item() {
     }
 
@@ -14,5 +30,9 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item(1, "Oleg");
     }
 }
