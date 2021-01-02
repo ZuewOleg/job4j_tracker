a32a4281f06a07a96cdc7c2e37c75ad6ff9106d5
ZuewOleg <zuev_17@bk.ru>
2021-01-02 17:19:27 +0500

    2. Модель данных. [#414150]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Zuev Oleg");
+        student.setGroup("D-07");
+        student.setReceipt(new Date());
+
+        System.out.println("Information about the student:" + System.lineSeparator() + "Name - " + student.getName() + System.lineSeparator() + "Group - " + student.getGroup() + System.lineSeparator() + "Date receipt - " + student.getReceipt());
+
+
+    }
+}
