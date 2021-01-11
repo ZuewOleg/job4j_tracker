f4d447591d91245cbe63e6c704c9fd06aa7931a7
ZuewOleg <zuev_17@bk.ru>
2021-01-11 18:00:10 +0500

    5. Tracker - хранилище [#414159]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,4 +47,14 @@ public class Tracker {
         }
         return rsl;
     }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return false;
+        } else {
+            items[index] = item;
+            return true;
+        }
+    }
 }
