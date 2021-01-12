c76f109106d612d3c22049c2d57a8209defb8d2a
ZuewOleg <zuev_17@bk.ru>
2021-01-12 15:31:29 +0500

    9. toString [#414158]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 public class Item {
     private int id;
+
     private String name;
 
     public Item(int id, String name) {
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,11 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI extends ItemNew {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
         Item item = new Item(1, "Oleg");
-        tracker.add(item);
-        tracker.findById(1);
-        System.out.println(tracker.findById(2));
         System.out.println(item);
     }
 }
