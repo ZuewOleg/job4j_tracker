310dbda49c91a66f757d02989a0c3a419ad3df2d
ZuewOleg <zuev_17@bk.ru>
2021-01-26 14:22:23 +0500

    1. Обеспечить бесперебойную работу приложения Tracker [#414106]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,6 +12,10 @@ public class StartUI {
         while (run) {
             this.showMenu(actions);
             int select = input.askInt("Select: ");
+            if (select < 0 || select >= actions.length) {
+                out.println("Wrong input, you can select: 0 ... " + (actions.length - 1));
+                continue;
+            }
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -26,12 +30,12 @@ public class StartUI {
 
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
-        Input input = new ConsoleInput();
+        Input input = new ValidateInput(); /* замена ConsoleInput */
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(output), new DeleteAction(output), new ReplaceAction(output),
                 new AddAction(output), new FindNameAction(output), new FindIdAction(output),
-                new ExitAction(output)/* another actions */
+                new ExitAction(output)
         };
         new StartUI(output).init(input, tracker, actions);
     }
