f3d4ee87f300da43f724956273c977e739921ae6
ZuewOleg <zuev_17@bk.ru>
2021-02-01 14:02:08 +0500

    0. Коллекции, ArrayList, List, Обобщения [#414064]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args) {
-        ArrayList<String> name = new ArrayList<String>();
+        ArrayList<String> name = new ArrayList<>();
         name.add("Petr");
         name.add("Ivan");
         name.add("Stepan");
