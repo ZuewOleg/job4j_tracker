e9290851f11a3a96e68efb78cead34eec238353d
ZuewOleg <zuev_17@bk.ru>
2021-02-03 17:18:03 +0500

    2. Идентичные тексты [#414072]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,20 +4,20 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = false;
+        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
         for (String originals : origin) { /* for-each origin -> new HashSet. */
             check.add(originals);
-            String[] a = originals.split(originals);
-            for (String texts : text) {/* for-each text -> hashSet.contains */
-                if (originals.contains(texts)) {
-                    rsl = true;
-                    break;
+                for (String texts : text) {/* for-each text -> hashSet.contains */
+                    if (check.contains(texts)) {
+                        rsl = true;
+                    } else {
+                        rsl = false;
+                    }
                 }
             }
-        }
         return rsl;
     }
 }
