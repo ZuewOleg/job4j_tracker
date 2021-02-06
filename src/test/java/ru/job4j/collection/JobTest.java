b337540d2b079b4640c9f5ece19a5e9e70427356
ZuewOleg <zuev_17@bk.ru>
2021-02-06 13:09:13 +0500

    2. Комбинированный компаратор [#414055]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import java.util.Comparator;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenCompatorByNameAndProrityDown() {
+        Comparator<Job> cmpNamePriority = new JobUpByPriority().thenComparing(new JobUpByName());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(4));
+    }
+
+    @Test
+    public void whenCompatorByName() {
+        Comparator<Job> cmpNamePriority = new JobDescByName();
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(1));
+    }
+
+    @Test
+    public void whenCompatorByPriority() {
+        Comparator<Job> cmpNamePriority = new JobUpByPriority();
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+}
