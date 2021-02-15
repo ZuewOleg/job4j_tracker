2c31f503b86672532564af0472e8f8809bebb6b8
ZuewOleg <zuev_17@bk.ru>
2021-02-15 11:27:15 +0500

    2.0. Встроенные функциональные интерфейсы. [#413884]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class SearchAtt {
-    public static List<Attachment> loop(List<Attachment> list, Predicate<List<Attachment>> func) {
+    public static List<Attachment> loop(List<Attachment> list, Predicate<Attachment> func) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
             if (func.test(att)) {
@@ -16,11 +16,15 @@ public class SearchAtt {
     }
 
     public static List<Attachment> filterSize(List<Attachment> list) {
-        Predicate<List<Attachment>> func = a -> a.getName().contains("bug");
+        for (Attachment a : list) {
+            Predicate<Attachment> func = a -> a.getName().contains("bug");
+        }
     }
 
     public static List<Attachment> filterName(List<Attachment> list) {
-        Predicate<List<Attachment>> func = a -> a.getSize() > 100;
+        for (Attachment a : list) {
+            Predicate<Attachment> func = a -> a.getSize() > 100;
+        }
     }
 
     return loop(list, func);
