2421dfd72f40503c09ee410275e8e5a20cf911fb
ZuewOleg <zuev_17@bk.ru>
2021-02-15 16:55:13 +0500

    5. Подсчет функции в диапазоне. [#413876]

--- src/main/java/ru/job4j/lambda/Funkcion.java
+++ src/main/java/ru/job4j/lambda/Funkcion.java
@@ -12,19 +12,4 @@ public class Funkcion {
         }
         return rsl;
     }
-
-    public static List<Double> linefunc(int start, int end) {
-            Function<Double, Double> func = x -> 2 * x + 1;
-        return diapason(start, end, func);
-    }
-
-    public static List<Double> squarefunc(int start, int end) {
-        Function<Double, Double> func = x -> Math.pow(x, 2D);
-        return diapason(start, end, func);
-    }
-
-    public static List<Double> indicativefunc(int start, int end) {
-        Function<Double, Double> func = x -> Math.pow(3, x);
-        return diapason(start, end, func);
-    }
 }
