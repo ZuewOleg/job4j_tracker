70363c3b9039dd03fbd8c652c14e88ffa752f4e2
ZuewOleg <zuev_17@bk.ru>
2021-02-16 19:51:52 +0500

    3. Уникальность элементов и сортировка. [#413893]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -7,4 +7,39 @@ public class Address {
     private String street;
     private int home;
     private int apartment;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getHome() {
+        return home;
+    }
+
+    public int getApartment() {
+        return apartment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return home == address.home && apartment == address.apartment
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
 }
--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,12 +1,17 @@
 package ru.job4j.stream;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.Comparator;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles){
+        Comparator<Address> comparator = (o1, o2) -> o1.getCity().compareTo(o2.getCity());
         return profiles.stream()
                 .map(profile -> profile.getAddress())
+                .sorted(comparator)
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
