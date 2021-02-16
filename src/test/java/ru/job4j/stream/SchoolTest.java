c29da97982f96230e6859806e9a14ab599abdf4d
ZuewOleg <zuev_17@bk.ru>
2021-02-16 18:57:49 +0500

    1. Фильтрация учеников [#413891]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -2,11 +2,9 @@ package ru.job4j.stream;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
