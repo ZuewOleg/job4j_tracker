775e961b19f3cc3dbb975687db1038263238e672
ZuewOleg <zuev_17@bk.ru>
2021-02-17 16:53:29 +0500

    9. Шаблоны проектирования в Stream API [#413899]

---
+++ src/main/java/ru/job4j/stream/Car.java
@@ -0,0 +1,85 @@
+package ru.job4j.stream;
+
+public class Car {
+    private String marka;
+    private String model;
+    private int year;
+    private double engine;
+    private String transmission;
+    private int powerhorse;
+
+    static class Builder {
+        private String marka;
+        private String model;
+        private int year;
+        private double engine;
+        private String transmission;
+        private int powerhorse;
+
+
+        Builder buildMarka(String marka) {
+            this.marka = marka;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildYear(int year) {
+            this.year = year;
+            return this;
+        }
+
+        Builder buildEngine(double engine) {
+            this.engine = engine;
+            return this;
+        }
+
+        Builder buildTransmission(String transmission) {
+            this.transmission = transmission;
+            return this;
+        }
+
+        Builder buildPowerHorse(int door) {
+            this.powerhorse = powerhorse;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.marka = marka;
+            car.model = model;
+            car.year = year;
+            car.engine = engine;
+            car.transmission = transmission;
+            car.powerhorse = powerhorse;
+            return car;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{"
+                + "marka='" + marka + '\''
+                + ", model='" + model + '\''
+                + ", year=" + year
+                + ", engine=" + engine
+                + ", transmission='" + transmission + '\''
+                + ", powerhorse=" + powerhorse
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        Car car = new Builder()
+                .buildMarka("BMW")
+                .buildModel("M3")
+                .buildYear(2020)
+                .buildEngine(5.6)
+                .buildTransmission("Auto")
+                .buildPowerHorse(350)
+                .build();
+        System.out.println(car);
+    }
+}
