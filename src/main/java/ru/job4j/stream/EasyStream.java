869065b03fc0c2df93b822387c4d31137f06d88f
ZuewOleg <zuev_17@bk.ru>
2021-02-21 17:53:10 +0500

    EasyStream

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -1,45 +1,68 @@
 package ru.job4j.stream;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.function.*;
 
 public class EasyStream {
     /** Нужно завести поле с типом List<Integer>
      * и писать в него результат работы промежуточных методов
      * и возвращать в качестве результата работы методов */
-    private static List<Integer> stream;
+    private List<Integer> stream;
+
+    /** чтобы запретить создание объектов класса вне этого класса */
+    private EasyStream() {}
+
+    /**  приватный конструктор, который будет инициализзировать поле
+     * и соответсвенно с его помощью мы будем писать значение в наше добавленное поле
+     */
+    private EasyStream stream(List<Integer> stream) {
+        this.stream = stream;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "EasyStream{"
+                +
+                "stream=" + stream
+                +
+                '}';
+    }
 
     /** of - получает исходные данные */
-    public static List<Integer> of(List<Integer> source) {
-        for (var s : source) {
-           stream.add(s);
-        }
-        return stream;
+    public static EasyStream of(List<Integer> source) {
+          EasyStream easyStream = new EasyStream().stream(source);
+        return easyStream;
     }
 
     /** map - преобразует число в другое число */
-    public List<Integer> map(Function<Integer, Integer> fun) {
-        List<Integer> rsl = new ArrayList<>(stream);
+    public EasyStream map(Function<Integer, Integer> fun) {
+        EasyStream rsl = new EasyStream();
         for (var st : stream) {
-            rsl.add(fun.apply(st));
-        }
+            stream.add(fun.apply(st));
+                rsl = new EasyStream().stream(stream);
+            }
         return rsl;
     }
 
     /** filter - фильтрует поток элементов */
-    public List<Integer> filter(Predicate<Integer> fun) {
-        List<Integer> rsl = new ArrayList<>(stream);
+    public EasyStream filter(Predicate<Integer> fun) {
+        EasyStream rsl = new EasyStream();
         for (var st : stream) {
             if (fun.test(st)) {
-                rsl.add(st);
+                stream.add(st);
             }
+            rsl = new EasyStream().stream(stream);
         }
         return rsl;
     }
 
     /** collect - собирает все элементы из source по заданным условиям map и filter */
     public List<Integer> collect() {
+        List<Integer> rsl = new ArrayList<>();
+        for (var s : stream) {
+            rsl.add(s);
+        }
+        return rsl;
     }
 }
